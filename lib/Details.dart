b852ded138c706cca4caafc58c1c4ac458c3f741
yaqoot9 <yaqootwishahi@gmail.com>
2023-04-16 22:39:54 -0700

    Sensor Indector

--- lib/Details.dart
+++ lib/Details.dart
@@ -1,8 +1,8 @@
 import 'package:firstapp/addProgram.dart';
 import 'package:firstapp/editProgram.dart';
 import 'package:firstapp/notification.dart';
-import 'package:firstapp/programs.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 class detailsPage extends StatelessWidget {
   const detailsPage({Key? key}) : super(key: key);
 
@@ -56,12 +56,74 @@ class _detailsState extends State<details> {
    return Center(
 
      child: Column(
+       mainAxisAlignment: MainAxisAlignment.start,
        children: [
-         //SizedBox(height: 20,),
+         SizedBox(height: 30,),
+         Card(
+             elevation: 20,child:
+         Column(children: [
+           SizedBox(height: 20),
+           Text("    Soil Moisture Sensor   ",style: TextStyle(fontWeight: FontWeight.bold ,fontSize: 16),),
+           SizedBox(height: 20,),
+           CircularPercentIndicator(
+             radius: 60.0,
+             lineWidth: 10.0,
+             percent: 0.6,
+             center: new Text("60%"),
+             progressColor: Colors.green,
+           ),
+             SizedBox(height: 20),
+         ],
+         )
+        ),
+
+         Row(
+           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+           children: [
+           Card(
+             elevation: 20,child:
+         Column(children: [
+           SizedBox(height: 20),
+           Text("    Humidity Sensor   ",style: TextStyle(fontWeight: FontWeight.bold ,fontSize: 16),),
+           SizedBox(height: 20,),
+           CircularPercentIndicator(
+             radius: 60.0,
+             lineWidth: 10.0,
+             percent: 0.7,
+             center: new Text("70%"),
+             progressColor: Colors.blue,
+           ),
+           SizedBox(height: 20),
+         ],
+         )
+         ),
+           Card(
+               elevation: 20,child:
+           Column(children: [
+             SizedBox(height: 20),
+             Text("    Tempretuer Sensor   ",style: TextStyle(fontWeight: FontWeight.bold ,fontSize: 16),),
+            SizedBox(height: 20,),
+             CircularPercentIndicator(
+               radius: 60.0,
+               lineWidth: 10.0,
+               percent: 0.4,
+               center: new Text("40%"),
+               progressColor: Colors.red,
+             ),
+            SizedBox(height: 20),
+           ],
+           )
+           ),
+         ],
+         ),
+
+
+      SizedBox(height: 55,),
+
        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: [
-         Container(color: Colors.lime[200],height: 30,child: TextButton(onPressed: () =>Navigator.pushNamed(context,'/addProgram',), child: Text('Add pump program',style: TextStyle(color: Colors.black87))),),
+         Container(color: Colors.lime[200],height: 40,child: TextButton(onPressed: () =>Navigator.pushNamed(context,'/addProgram',), child: Text('Add pump program',style: TextStyle(color: Colors.black87))),),
          Container(
-           color: Colors.lime[200],height: 30,
+           color: Colors.lime[200],height: 40,
            child:TextButton(onPressed:()=>Navigator.pushNamed(context,'/EditProgram',), child: Text('Edit pump program ',style: TextStyle(color: Colors.black87),)),
          )
        ],)
